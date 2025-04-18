01f3df14ba470ab8b74e81872187f890881f68ef
Clara Rigaud <clararigaud0@gmail.com>
2025-04-18 19:15:44 +0200

    compatible android 2 firefox new interfaces

--- index.js
+++ index.js
@@ -1,18 +1,4 @@
 const Zombitron = require("./zombitron/server/Zombitron");
 const zombitron = new Zombitron();
-
-zombitron.app.use('/assets', zombitron.express.static(__dirname + '/assets'));
-
-zombitron.app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
-});
-
-zombitron.app.get('/interface1', function (req, res) {
-  res.sendFile(__dirname + '/views/interface1.html');
-});
-
-zombitron.app.get('/interface2', function (req, res) {
-  res.sendFile(__dirname + '/views/interface2.html');
-});
-
+// You can do what you want here (server side)
 zombitron.start();
